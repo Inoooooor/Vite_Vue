052eaf7afab86f15114428f5d245447a6e2cf929
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2022-12-06 16:20:43 +0300

    getting_along_with_Vite_Vue

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script>
+import { createApp } from 'vue'
+
+export default {
+  data() {
+    return {
+      message: 'САлам Вью '
+    }
+  },
+  methods: {
+    reverseMessage() {
+      this.message = this.message
+        .split('')
+        .reverse()
+        .join('')
+    }
+  }
+}
+
+</script>
+
+<template>
+  <div id="reverse">
+    <p>{{ message }}</p>
+    <button v-on:click="reverseMessage">press me to reverse</button>
+  </div>
+</template>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+.greeting {
+  color: rebeccapurple;
+  font-weight: bold;
+}
+
+#reverse {
+  height: 100vh;
+  width: 100wh;
+  background-color: aqua;
+}
+</style>
